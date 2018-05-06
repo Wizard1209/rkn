496878f8ff19f1b18668d6567deee8cdf8abae8e
Wizard1209 <34334729+Wizard1209@users.noreply.github.com>
2018-05-06 20:34:10 +0300

    Update perfomance comparison

--- speed_test.py
+++ speed_test.py
@@ -1,10 +1,12 @@
 '''Через дерево немного быстрее
+Через заранее сгенерированный массив битов сильно быстрее
 примерно половину времени занимает преобразование ip в двоичную запись
 '''
 from timeit import timeit
 
 from rkn import RKN
 from rkn1 import RKN1
+from rkn2 import RKN2
 
 stmt = '''
 r.is_banned('87.251.23.31')
@@ -18,10 +20,14 @@ r.is_banned('255.168.1.1')
 r.is_banned('255.168.0.1')
 '''
 
+r = RKN(['87.250.0.0/15', '8.8.8.8/8', '192.168.0.1/31'])
+print(timeit(stmt=stmt, number=200000, globals={'r': locals().get('r')}))
+# 22.5 секунды у меня
+
 r = RKN1(['87.250.0.0/15', '8.8.8.8/8', '192.168.0.1/31'])
-print(timeit(stmt=stmt, number=100000, globals={'r': locals().get('r')}))
+print(timeit(stmt=stmt, number=200000, globals={'r': locals().get('r')}))
 # 20 секунд у меня
 
-r = RKN(['87.250.0.0/15', '8.8.8.8/8', '192.168.0.1/31'])
-print(timeit(stmt=stmt, number=100000, globals={'r': locals().get('r')}))
-# 22.5 секунды у меня
+r = RKN2(['87.250.0.0/15', '8.8.8.8/8', '192.168.0.1/31'])
+print(timeit(stmt=stmt, number=200000, globals={'r': locals().get('r')}))
+# 6.5 секунд у меня
